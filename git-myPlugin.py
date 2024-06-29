0bc59893ac2ad6226c0bc406d3a6dcf880c36105
b-barrientos <bryan.barrientos83@gmail.com>
2024-06-29 13:52:11 -0500

    Finished git-myPlugin

---
+++ git-myPlugin.py
@@ -0,0 +1,36 @@
+#!/urs/bin/env python3
+
+import click
+import os
+import subprocess
+
+@click.command()
+def run():
+    print("Hello")
+
+if __name__ == '__main__':
+    run()
+
+def _walk_dir(dir):
+    """Walk through dir and return true if we find a directory labelled .git, 
+    otherwise return false"""
+    for filename in os.listdir(dir):
+        f = os.path.join(dir, filename)
+        if os.path.isdir(dir) == True:
+            if filename.basename == ".git":
+                return True
+        return False
+
+def _run_git_commands(repo_dir):
+    """Runs the following commands in order on the repo given"""
+    subprocess.run(["git", "pull"])
+    subprocess.run(["git", "push"])
+    subprocess.run(["git", "push", "--tags"])
+    
+
+@click.command()
+@click.option('-d', '--dir', default=".", type=str, help="Target path to scan")
+def run(dir):
+    is_git = _walk_dir(dir)
+    if is_git is True:
+        _run_git_commands(dir)
