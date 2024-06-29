f0d14788b87e53f42b65d1c1e79350187046be72
b-barrientos <bryan.barrientos83@gmail.com>
2024-06-29 14:14:20 -0500

    Fixed git-myPlugin. Removed basename from file

--- git-myPlugin.py
+++ git-myPlugin.py
@@ -4,22 +4,16 @@ import click
 import os
 import subprocess
 
-@click.command()
-def run():
-    print("Hello")
-
-if __name__ == '__main__':
-    run()
-
 def _walk_dir(dir):
     """Walk through dir and return true if we find a directory labelled .git, 
     otherwise return false"""
     for filename in os.listdir(dir):
         f = os.path.join(dir, filename)
         if os.path.isdir(dir) == True:
-            if filename.basename == ".git":
+            print("Filename: " + filename)
+            if filename == ".git":
                 return True
-        return False
+    return False
 
 def _run_git_commands(repo_dir):
     """Runs the following commands in order on the repo given"""
@@ -34,3 +28,6 @@ def run(dir):
     is_git = _walk_dir(dir)
     if is_git is True:
         _run_git_commands(dir)
+
+if __name__ == '__main__':
+    run()
